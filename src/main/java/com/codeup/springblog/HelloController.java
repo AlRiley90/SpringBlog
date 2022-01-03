3e8fd72572f7954770eb4a6223ca75da482bde97
Alex Riley <alex.riley.ar12@gmail.com>
2022-01-03 12:26:05 -0600

    incorporated thymeleaf into our spring pages for views

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,7 +1,10 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,5 +14,16 @@ class HelloController {
     @ResponseBody
     public String hello() {
         return "Hello from Spring!";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm(){
+        return "join";
+    }
+
+    @PostMapping("join")
+    public String joinCohort(@RequestParam(name="cohort") String cohort, Model model){
+    model.addAttribute("cohort", cohort);
+    return "join";
     }
 }
